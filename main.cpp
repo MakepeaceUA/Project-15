60eea86e221eb0e97ab9bdb515b41748b4a211e4
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-07-02 19:03:13 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+// Задание 1
+//int main()
+//{
+//    srand(time(0));
+//    setlocale(0, "RUS");
+
+//    int list1[10];
+//    int list2[5];
+//    int list3[5];
+
+//    for (int i = 0; i < 10; i++)
+//    {
+//        list1[i] = rand() % 100;
+//        cout << list1[i] << " ";
+//    }
+//    cout << "\n\n";
+//    for (int i = 0; i < 5; i++)
+//    {
+//        list2[i] = list1[i];
+//        list3[i] = list1[i + 5];
+//    }
+
+//    for (int i = 0; i < 5; i++)
+//    {
+//        cout << list2[i] << " ";
+//    }
+//    cout << "\n\n";
+//    for (int i = 0; i < 5; i++)
+//    {
+//        cout << list3[i] << " ";
+//    }
+//}
+
+
+
+
+
+// Задание 2
+//int main()
+//{
+//    srand(time(0));
+//    setlocale(0, "RUS");
+
+//   int list1[10];
+//    int list2[10];
+    
+//    int list3[10];
+
+//    for (int i = 0; i < 10; i++)
+//    {
+//        list1[i] = rand() % 100;
+//        list2[i] = rand() % 100;
+//    }
+//    cout << "\n\n";
+//    for (int i = 0; i < 10; i++)
+//    {
+//        cout << list1[i] << " ";
+//    }
+//    cout << "\n\n";
+//    for (int i = 0; i < 10; i++)
+//    {
+//        cout << list2[i] << " ";
+//   }
+//     cout << "\n\n";
+  
+//    for (int i = 0; i < 10; i++)
+//    {
+//        list3[i] = list1[i] + list2[i];
+//        cout << list3[i] << " ";
+//    }
+//}
+
+
+
+
+// Задание 3
+//int main()
+//{
+//    srand(time(0));
+//    setlocale(0, "RUS");
+
+//    int list[7];
+//    int sum = 0;
+//    int day = 0;
+//    for (int i = 1;i < 8;i++) 
+//    {
+//      cout << "Введите расходы в $ за " << i << " день недели:\n";
+//      cin >> list[i];
+//      sum += list[i];
+//      if (list[i] > 100) 
+//      {
+//        day++;
+//      }
+//    }
+//    cout << "\n\n";
+//    cout << "Средняя потраченная сумма за неделю: " << sum / 7 << "\n";
+//    cout << "Общая сумма расходов за неделю: " << sum << "\n";
+//    cout << "Сумма дней,в которых расходы превысили 100$: " << day << "\n";
+//}
+
+
+
+
+
+
+
+
+
+
+
+
